7b3d42354dad6abd0fe4f0d9ae6990dfd0d7b3a8
Arthur <artur_lyup@mail.ru>
2018-06-24 03:52:53 +0300

    Were fixed some moments

--- Task04/Coin.java
+++ Task04/Coin.java
@@ -4,7 +4,7 @@ package task_04;
  * Coin is the class which solves the following task:
  * Toss a coin any times and count number of reverses and obverses.
  *
- * 18 June 2018
+ * 24 June 2018
  * @author Arthur Lyup
  */
 
@@ -14,7 +14,9 @@ public class Coin {
     private static final int FIRST_TOSS = 1;
 
     public static String tossCoin(int number) throws Exception {
-        checkNumber(number);//check input
+        if (number < FIRST_TOSS){
+            throw new Exception("Wrong input! Number of tosses must be > 0");
+        }
         Random random = new Random();
         int numberOfObverses = 0;
         for (int i = FIRST_TOSS; i <= number; i++){
@@ -24,11 +26,5 @@ public class Coin {
         }
         return number + " tosses of the coin: " + numberOfObverses + " obverses and " + (number - numberOfObverses)
                 + " reverses";
-    }
-
-    private static void checkNumber(int number) throws Exception {
-        if (number < FIRST_TOSS){
-            throw new Exception("Wrong input! Number of tosses must be > 0");
-        }
     }
 }
